5791d19ea1b88254aa0597a2f98a8f8540c6598a
FarahKamleh <fkamleh8800@gmail.com>
2024-04-19 16:33:38 -0500

    Audio Toggles
    User can switch between One More Light and Stars, but the toggles do not operate as intended. To have them work, the one playing needs to be turned off before the next is played.

---
+++ CS 528 - Shining Star - Farah Kamleh/Assets/Scripts/audioChanger.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioChanger : MonoBehaviour
+{
+    // refer to both audio clips that will be switched
+    public AudioClip LinkinPark;
+    public AudioClip Skillet;
+
+    // the audio source to be used
+    public AudioSource smAudioSource;
+
+    // toggles for the above
+    public Toggle light;
+    public Toggle stars;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // make sure One More Light plays at the start
+        light.isOn = true;
+    }
+
+    // FIXME: a function that will be called for toggle presses
+    public void SwitchAudio()
+    {
+        // if Light is on, play audio
+        if (light.isOn == true)
+        {
+            // stop the audio source
+            // smAudioSource.Stop();
+
+            // turn the other toggle off
+            stars.isOn = false;
+
+            // play the correct song
+            smAudioSource.PlayOneShot(LinkinPark);
+        }
+
+        // if Stars toggle is on, play audio
+        if (stars.isOn == true)
+        {
+            // stop the audio source
+            // smAudioSource.Stop();
+
+            // turn the other toggle off
+            light.isOn = false;
+
+            // play the correct song
+            smAudioSource.PlayOneShot(Skillet);
+        }
+
+        // if all toggles are off, 
+        else if ((light.isOn == false) && (stars.isOn == false))
+        {
+            smAudioSource.Stop();
+        }
+    }
+}
